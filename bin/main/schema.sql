1a85c148e706c60f62b10208b139e900ef5ecb91
thinh.vh <thinh.vh@inext.vn>
2022-11-30 16:38:39 +0700

    upload survey

---
+++ bin/main/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS resources
+(
+    id              bigserial PRIMARY KEY,
+    fhir_id         varchar(64) NOT NULL,
+    type            varchar(64) NOT NULL,
+    data            jsonb       NOT NULL,
+    created_at      timestamp   NOT NULL DEFAULT NOW(),
+    last_updated_at timestamp   NOT NULL DEFAULT NOW(),
+    is_deleted      boolean     NOT NULL DEFAULT FALSE,
+    CONSTRAINT fhir_id_unique UNIQUE (fhir_id, type)
+);
+
+CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS resource_id_idx ON resources (id);
+CREATE INDEX IF NOT EXISTS resource_type_idx ON resources (type);
+CREATE INDEX IF NOT EXISTS last_updated_at_idx ON resources (last_updated_at DESC);
